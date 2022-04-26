f183f8221b592c40549b08ecb33ea6dccdda1bc0
khushi4632 <103728967+khushi4632@users.noreply.github.com>
2022-04-26 23:13:03 +0530

    Create basic data types.cpp

---
+++ day 2/basic data types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int a;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld  %c %f  %lf",&a,&l,&c,&f,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf\n",a,l,c,f,d);  
+    
+    return 0;
+}
