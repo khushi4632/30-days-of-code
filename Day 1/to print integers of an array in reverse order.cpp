b35e1b4ed0554a28c90d1005ccd7c56221d1f1de
khushi4632 <103728967+khushi4632@users.noreply.github.com>
2022-04-26 16:07:13 +0530

    Create to print integers of an array in reverse order.cpp

---
+++ Day 1/to print integers of an array in reverse order.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin>>N;
+    
+   int a[N];
+   for (int i = 0; i < N; i++)
+    {
+        cin >> a[i];   
+    }     
+    
+    for (int j = N - 1; j >= 0; j--) 
+    {
+        cout << a[j] << " ";
+    } 
+    return 0;
+}
